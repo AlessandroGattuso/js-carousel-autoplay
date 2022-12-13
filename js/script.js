c7a1700c9d93ba32257868f9fcf62e9528f0303b
Alessandro <alessandrogattuso@yahoo.it>
2022-12-13 11:06:20 +0100

    change 3 sec to 3.5 sec

--- js/script.js
+++ js/script.js
@@ -106,10 +106,11 @@ autoplay.addEventListener("click", function(){
     }
 });
 
+
 //Se il mouse non è sopra il bottone, il bottone scompare
 autoplay.addEventListener('mouseleave', function(event){
     if(event)
-         setTimeout(()=>this.style.opacity = '0', 3000);
+         setTimeout(()=>this.style.opacity = '0', 3500);
 });
 
 //Se il mouse invece entra nell'area del bottone questo appare
@@ -117,7 +118,6 @@ autoplay.addEventListener('mouseenter', function(event){
     if(event)
          this.style.opacity = '1';
 });
-
 
 
 function scroll_Right_Down(){
